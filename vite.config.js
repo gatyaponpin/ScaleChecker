168020896f69814b67fa75c23751cba3509eff87
e_watanabe@exscion.co.jp <e_watanabe@exscion.co.jp>
2025-10-03 18:14:48 +0900

    chore:1ファイルでビルドされるようconfig修正

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 import path from 'node:path'
 
 export default defineConfig({
+  base: './',  
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
+    viteSingleFile(),
   ],
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') },
+  },
+  build: {
+    cssCodeSplit: false, // CSSを分割しない
+    assetsInlineLimit: 100000000, // アセットも極力インライン化（大きめに）
+    modulePreload: false,
   },
 })
